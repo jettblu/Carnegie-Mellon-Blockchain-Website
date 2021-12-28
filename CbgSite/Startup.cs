78bb1bc21d55d426250a9130623906accc8910b7
jettblu <jettblu.fabrications@gmail.com>
2021-12-27 19:01:41 -0800

    Add table style

--- CbgSite/Startup.cs
+++ CbgSite/Startup.cs
@@ -30,8 +30,8 @@ namespace CbgSite
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            /*services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();*/
             services.AddRazorPages();
         }
 
