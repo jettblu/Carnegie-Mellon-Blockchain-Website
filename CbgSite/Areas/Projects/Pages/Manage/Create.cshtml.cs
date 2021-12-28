72478960e822535d1919b0af2108afa213772e74
jettblu <jettblu.fabrications@gmail.com>
2021-12-28 14:42:26 -0800

    Add routines for project creation

--- CbgSite/Areas/Projects/Pages/Manage/Create.cshtml.cs
+++ CbgSite/Areas/Projects/Pages/Manage/Create.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CbgSite.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,32 @@ namespace CbgSite.Areas.Projects.Pages.Manage
 {
     public class CreateModel : PageModel
     {
+        private Services.ProjectManager _projectManager { get; set; }
+        private CbgSiteContext _contextCbg { get; set; }
+        public CreateModel(Services.ProjectManager projectManager, CbgSiteContext contextCbg)
+        {
+            _projectManager = projectManager;
+            _contextCbg = contextCbg;
+        }
+        [BindProperty]
+        public Data.Project Project { get; set; }
         public void OnGet()
         {
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _contextCbg.Projects.Add(Project);
+            await _contextCbg.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
