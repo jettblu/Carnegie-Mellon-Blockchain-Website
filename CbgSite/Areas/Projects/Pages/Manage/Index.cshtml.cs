81e848a96674a266be3aa7ebc1278e23a9a696cc
jettblu <jettblu.fabrications@gmail.com>
2021-12-27 19:38:49 -0800

    Add basic project tabel

--- CbgSite/Areas/Projects/Pages/Manage/Index.cshtml.cs
+++ CbgSite/Areas/Projects/Pages/Manage/Index.cshtml.cs
@@ -9,8 +9,15 @@ namespace CbgSite.Areas.Projects.Pages.Manage
 {
     public class IndexModel : PageModel
     {
+        private Services.ProjectManager _projectManager { get; set; }
+        public IndexModel(Services.ProjectManager projectManager)
+        {
+            _projectManager = projectManager;
+        }
+        public List<Data.Project> Projects { get; set; }
         public void OnGet()
         {
+            Projects = _projectManager.GetProjects();
         }
     }
 }
