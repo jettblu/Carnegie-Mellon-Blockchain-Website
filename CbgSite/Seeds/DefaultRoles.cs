a0b4fe8db58e2d6a86fd29baed7cdc74d62d41f9
jettblu <jettblu.fabrications@gmail.com>
2022-01-03 12:05:16 -0800

    Add application roles seed

---
+++ CbgSite/Seeds/DefaultRoles.cs
@@ -0,0 +1,19 @@
+﻿using CbgSite.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CbgSite.Seeds
+{
+    public class DefaultRoles
+    {
+        public static async Task SeedAsync(UserManager<CbgUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            await roleManager.CreateAsync(new IdentityRole(Globals.Roles.SuperAdmin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Globals.Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Globals.Roles.Basic.ToString()));
+        }
+    }
+}
