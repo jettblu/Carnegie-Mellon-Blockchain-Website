c7d1b504b8002665817f1da2fa3128288768ba7f
jettblu <jettblu.fabrications@gmail.com>
2022-01-03 12:06:12 -0800

    Add globals and role types

---
+++ CbgSite/Globals.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CbgSite
+{
+    public class Globals
+    {
+        public enum Roles
+        {
+            SuperAdmin,
+            Admin,
+            Basic
+        }
+    }
+}
