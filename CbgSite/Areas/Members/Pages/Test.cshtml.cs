e910a9deae2f1fa66b7c82507a775971410746fb
jettblu <jettblu.fabrications@gmail.com>
2022-01-07 12:13:48 -0800

    Add basic view modules to member and project sections

--- CbgSite/Areas/Members/Pages/Test.cshtml.cs
+++ CbgSite/Areas/Members/Pages/Test.cshtml.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CbgSite.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CbgSite.Areas.Members.Pages
 {
+    [Authorize(Roles = "SuperAdmin")]
     public class TestModel : PageModel
     {
         private readonly UserManager<CbgUser> _userManager;
@@ -33,10 +35,11 @@ namespace CbgSite.Areas.Members.Pages
         {
         }
 
-        public async void OnPost()
+        public async Task OnPostAddRole()
         {
             var user = await _userManager.GetUserAsync(User);
-            await _userManager.AddToRoleAsync(user, Globals.Roles.SuperAdmin.ToString());
+            var role = Globals.Roles.SuperAdmin.ToString();
+            var res = await _userManager.AddToRoleAsync(user, role);
         }
     }
 }
