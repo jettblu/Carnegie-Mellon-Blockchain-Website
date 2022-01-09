5e532eb5b774274168785c795665a3ef2ae6a652
jettblu <jettblu.fabrications@gmail.com>
2022-01-08 23:10:06 -0800

    Add auth. dependencies

--- CbgSite/Areas/Members/Pages/Index.cshtml.cs
+++ CbgSite/Areas/Members/Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CbgSite.Areas.Identity.Data;
 using CbgSite.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
