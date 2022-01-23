e788059158f5b5678f534cf7e5d8698b3c9c80ca
jettblu <jettblu.fabrications@gmail.com>
2022-01-22 20:03:37 -0800

    Add role management for all members

--- CbgSite/Areas/Members/Pages/Index.cshtml.cs
+++ CbgSite/Areas/Members/Pages/Index.cshtml.cs
@@ -26,11 +26,31 @@ namespace CbgSite.Areas.Members.Pages
             _signInManager = signInManager;
             _contextCbg = cbgSiteContext;
         }
+        [TempData]
+        public string StatusMessage { get; set; }
+        [BindProperty]
         public List<CbgUser> Members { get; set; }
+        [BindProperty]
+        public Globals.Roles MemberRole{ get; set; }
+        [BindProperty]
+        public string MemberId { get; set; }
         public void OnGet()
         {
             // retrieve members and update model state          
             Members = _contextCbg.Users.ToList();
+        }
+        // ADD ABILITY TO REMOVE FROM ALL ROLES (or display multiple at same time)
+        public async Task<JsonResult> OnPostUpdateRolesAsync()
+        {
+            var user = await _userManager.FindByIdAsync(MemberId);
+            // remove all roles from user
+            var roles = await _userManager.GetRolesAsync(user);
+            await _userManager.RemoveFromRolesAsync(user, roles.ToArray());
+            // add user to new role
+            var res = await _userManager.AddToRoleAsync(user, MemberRole.ToString());
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Member Role Updated!";
+            return new JsonResult("Role function completed");
         }
     }
 }
---
+++ CbgSite/wwwroot/js/MemberManager.js
@@ -0,0 +1,17 @@
+﻿var UpdateRole = function () {
+    var id = $(this).data("id");
+    console.log(id);
+    var role = $(this).val();
+    $("#memberId").val(id);
+    $("#memberRole").val(role);
+    console.log("Submitting role change request....");
+    $("#memberRoleForm").submit();
+    console.log("Submitted!");
+    console.log($("#memberId").val());
+}
+
+var completeRole = function (res) {
+    console.log("Role Update Completed");
+}
+
+$(".memberRole").on('change', UpdateRole);
