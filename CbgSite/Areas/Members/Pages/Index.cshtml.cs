2a836dced36ee93d4bddc5f4cc7ea277cdb3923f
jettblu <jettblu.fabrications@gmail.com>
2022-01-30 19:26:28 -0500

    Add link between member profile and main member page

---
+++ CbgSite/Areas/Members/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CbgSite.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CbgSite.Areas.Members.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private CbgSiteContext _contextCbg { get; set; }
+        public IndexModel(CbgSiteContext contextCbg)
+        {
+            _contextCbg = contextCbg;
+        }
+        [BindProperty]
+        public Identity.Data.CbgUser User { get; set; }
+        public async Task<IActionResult> OnGet(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User = _contextCbg.Users.FirstOrDefault(u => u.Id == id);
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
