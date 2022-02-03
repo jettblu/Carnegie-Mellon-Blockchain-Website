43d17d497053424667fedd1f537135348f2700b1
jettblu <jettblu.fabrications@gmail.com>
2022-02-02 19:03:20 -0500

    Add quick link to project editor

--- CbgSite/Areas/Projects/Pages/Index.cshtml.cs
+++ CbgSite/Areas/Projects/Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CbgSite.Areas.Identity.Data;
 using CbgSite.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -13,13 +14,17 @@ namespace CbgSite.Areas.Projects.Pages
     {
         private Services.ProjectManager _projectManager { get; set; }
         private CbgSiteContext _contextCbg { get; set; }
-        public IndexModel(Services.ProjectManager projectManager, CbgSiteContext contextCbg)
+        private UserManager<CbgUser> _userManager { get; set; }
+        public IndexModel(Services.ProjectManager projectManager, CbgSiteContext contextCbg, UserManager<CbgUser> userManager)
         {
             _projectManager = projectManager;
             _contextCbg = contextCbg;
+            _userManager = userManager;
         }
         [BindProperty]
         public Data.Project Project { get; set; }
+        [BindProperty]
+        public bool IsCreator { get; set; }
         [BindProperty]
         public List<CbgUser> ProjectUsers { get; set; }
         public async Task<IActionResult> OnGet(string id)
@@ -28,10 +33,17 @@ namespace CbgSite.Areas.Projects.Pages
             {
                 return NotFound();
             }
-
+            var user = await _userManager.GetUserAsync(User);
             Project = _contextCbg.Projects.FirstOrDefault(p => p.Id == id);
             ProjectUsers = await _projectManager.GetProjectUsers(Project);
             
+            // toggle authorization based on custom parameters
+            var isSuperAdmin = await _userManager.IsInRoleAsync(user, Globals.Roles.SuperAdmin.ToString());
+            if (isSuperAdmin || ProjectUsers.Contains(user))
+            {
+                IsCreator = true;
+            }
+
             if (Project == null)
             {
                 return NotFound();
